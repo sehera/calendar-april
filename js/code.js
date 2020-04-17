2ac2ad75697380d394949a0b2fe04a91479b2809
sehera <55153575+sehera@users.noreply.github.com>
2020-04-17 15:36:55 -0400

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+  $("#backgcolor").on("click", function (){
+  $("body").css("background-color", "#AE95C4")
+  });
+
+
+
+$( function() {
+$( "#draggable" ).draggable();
+  });
+
+  $( function() {
+  $( "#draggable2" ).draggable();
+    });
+
+    $( function() {
+    $( "#draggable3" ).draggable();
+      });
+
+      $( function() {
+      $( "#draggable4" ).draggable();
+        });
+
+        $( function() {
+        $( "#draggable5" ).draggable();
+          });
+
+          $( function() {
+          $( "#draggable6" ).draggable();
+            });
+
+            $( function() {
+            $( "#draggable7" ).draggable();
+              });
+
+              $( function() {
+              $( "#draggable8" ).draggable();
+                });
+
+                $( function() {
+                $( "#draggable9" ).draggable();
+                  });
+
+                  $( function() {
+                  $( "#draggable10" ).draggable();
+                    });
+
+                    $( function() {
+                    $( "#draggable11" ).draggable();
+                      });
+
+                      $( function() {
+                      $( "#draggable12" ).draggable();
+                        });
+
+                        $( function() {
+                        $( "#draggable13" ).draggable();
+                          });
+
+                          $( function() {
+                          $( "#draggable14" ).draggable();
+                            });
+
+                            $( function() {
+                            $( "#draggable15" ).draggable();
+                              });
+
+                              $( function() {
+                              $( "#draggable16" ).draggable();
+                                });
+
+                                $( function() {
+                                $( "#draggable17" ).draggable();
+                                  });
+
+                                  $( function() {
+                                  $( "#draggable18" ).draggable();
+                                    });
+
+                                    $( function() {
+                                    $( "#draggable19" ).draggable();
+                                      });
+
+                                      $( function() {
+                                      $( "#draggable20" ).draggable();
+                                        });
+
+                                        $( function() {
+                                        $( "#draggable21" ).draggable();
+                                          });
+
+                                          $( function() {
+                                          $( "#draggable22" ).draggable();
+                                            });
+
+                                            $( function() {
+                                            $( "#draggable23" ).draggable();
+                                              });
+
+                                              $( function() {
+                                              $( "#draggable24" ).draggable();
+                                                });
+
+                                                $( function() {
+                                                $( "#draggable25" ).draggable();
+                                                  });
+
+                                                  $( function() {
+                                                  $( "#draggable26" ).draggable();
+                                                    });
+
+                                                    $( function() {
+                                                    $( "#draggable27" ).draggable();
+                                                      });
+
+                                                      $( function() {
+                                                      $( "#draggable28" ).draggable();
+                                                        });
+
+                                                        $( function() {
+                                                        $( "#draggable29" ).draggable();
+                                                          });
+
+                                                          $( function() {
+                                                          $( "#draggable30" ).draggable();
+                                                            });
+
+                                                            $( function() {
+                                                            $( "#draggable31" ).draggable();
+                                                              });
+
+
+})
